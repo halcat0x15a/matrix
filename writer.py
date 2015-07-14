ac343f57ffb96b358d78c54125131921c764496c
Sanshiro Yoshida <halcat0x15a@gmail.com>
2015-07-14 18:22:12 +0900

    breakable

--- writer.py
+++ writer.py
@@ -15,15 +15,17 @@ input = sys.argv[1]
 output = sys.argv[2]
 cap = cv2.VideoCapture(input)
 fourcc = cv2.cv.CV_FOURCC(*'XVID')
-out = cv2.VideoWriter(output, 0, 20.0, (width, height))
+out = cv2.VideoWriter(output, 0, 20.0, size)
 
 try:
     while(cap.isOpened()):
         ret, frame = cap.read()
+        print(cap.get(cv2.cv.CV_CAP_PROP_POS_MSEC))
         if ret:
             frame = cv2.resize(frame, size)
             out.write(frame)
+        else:
+            break
 finally:
     cap.release()
     out.release()
-
