ff0cdbd0c35715419141482ebcd888a237db1eb3
Sanshiro Yoshida <halcat0x15a@gmail.com>
2015-07-21 08:45:03 +0000

    update matrix param

--- matrix.py
+++ matrix.py
@@ -2,11 +2,11 @@ from rgbmatrix import RGBMatrix
 
 rows = 16
 cols = 32
-chains = 1
-parallel = 1
+chains = 3
+parallel = 3
 width = cols * chains
 height = rows * parallel
-size = (height, width)
+size = (width, height)
 
 matrix = RGBMatrix(rows, chains, parallel)
 
