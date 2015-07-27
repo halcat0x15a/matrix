8d61dd8cdba9762e231d39fa512f760852c0fa85
Sanshiro Yoshida <halcat0x15a@gmail.com>
2015-07-28 00:10:30 +0900

    rename js to joystick

--- tetris.py
+++ tetris.py
@@ -2,7 +2,7 @@ import pygame
 import random
 import threading
 import matrix
-import js
+import joystick
 
 BLACK = (0, 0, 0)
 
@@ -13,7 +13,7 @@ class Block:
 
         def __init__(self, shape, color, x, y):
                 self.shape = shape
-		self.color = color
+                self.color = color
                 self.x = x
                 self.y = y
 
@@ -49,53 +49,54 @@ SHAPES = (I, T, Z, S, O, L, J)
 COLORS = ((255, 0, 0), (0, 255, 255), (0, 255, 0), (255, 0, 255), (255, 255, 0), (255, 127, 0), (0, 0, 255))
 
 def newblock(x, y):
-	n = random.randint(0, len(SHAPES) - 1)
+        n = random.randint(0, len(SHAPES) - 1)
         shape = SHAPES[n]
-	color = COLORS[n]
+        color = COLORS[n]
         return Block(shape, color, x, y)
 
 def show(display):
-	canvas = matrix.matrix.CreateFrameCanvas()
-	for x, line in enumerate(display):
-		for y, pixel in enumerate(line):
-			for i in range(matrix.height / WIDTH):
-				for j in range(matrix.width / HEIGHT):
-					if pixel != BLACK:
- 						r, g, b = pixel
-						canvas.SetPixel(x * matrix.height / WIDTH + i, y * matrix.width / HEIGHT + j, r, g, b)
-	matrix.matrix.SwapOnVSync(canvas)
+        canvas = matrix.matrix.CreateFrameCanvas()
+        for x, line in enumerate(display):
+                for y, pixel in enumerate(line):
+                        for i in range(matrix.height / WIDTH):
+                                for j in range(matrix.width / HEIGHT):
+                                        if pixel != BLACK:
+                                                r, g, b = pixel
+                                                canvas.SetPixel(x * matrix.height / WIDTH + i, y * matrix.width / HEIGHT + j, r, g, b)
+        matrix.matrix.SwapOnVSync(canvas)
 
 def clear(display):
-	bottom = tuple(line for line in display if not all(pixel != BLACK for pixel in line))
-	top = ((BLACK,) * WIDTH,) * (HEIGHT - len(bottom))
-	return top + bottom
+        bottom = tuple(line for line in display if not all(pixel != BLACK for pixel in line))
+        top = ((BLACK,) * WIDTH,) * (HEIGHT - len(bottom))
+        return top + bottom
 
 def main():
         current = None
         display = ((BLACK,) * WIDTH,) * HEIGHT
         clock = pygame.time.Clock()
         counter = 0
-	(process, queue) = js.queue()
-	process.start()
+        (process, queue) = joystick.queue()
+        process.start()
+        buttons = {'05':False,'07':False,'06':False,'0D':False,'0E':False}
         while True:
-		clock.tick(30)
+                clock.tick(30)
                 if current == None:
                         current = newblock(WIDTH / 2, 0)
                         if current.check(display):
                                 break
-		while not queue.empty():
-			(pressed, button) = queue.get()
-			if pressed:
-				if button == '05' and not current.move(-1, 0).check(display):
-					current = current.move(-1, 0)
-				elif button == '07' and not current.move(1, 0).check(display):
-					current = current.move(1, 0)
-				elif button == '06' and not current.move(0, 1).check(display):
-					current = current.move(0, 1)
-				elif button == '0D' and not current.turn(1).check(display):
-					current = current.turn(1)
-				elif button == '0E' and not current.turn(-1).check(display):
-					current = current.turn(-1)
+                while not queue.empty():
+                        (pressed, button) = queue.get()
+                        buttons[button] = pressed
+                if buttons['05'] and not current.move(-1, 0).check(display):
+                        current = current.move(-1, 0)
+                elif buttons['07'] and not current.move(1, 0).check(display):
+                        current = current.move(1, 0)
+                elif buttons['06'] and not current.move(0, 1).check(display):
+                        current = current.move(0, 1)
+                elif buttons['0D'] and not current.turn(1).check(display):
+                        current = current.turn(1)
+                elif buttons['0E'] and not current.turn(-1).check(display):
+                        current = current.turn(-1)
                 if counter < 10:
                         counter += 1
                 else:
@@ -105,10 +106,10 @@ def main():
                         else:
                                 current = current.move(0, 1)
                         counter = 0
-		if current:
-                	show(current.fix(display))
-		else:
-			show(display)
+                if current:
+                        show(current.fix(display))
+                else:
+                        show(display)
 
 if __name__ == '__main__':
        main()
